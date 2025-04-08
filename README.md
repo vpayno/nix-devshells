21d6907ef04788719341959d735e228574e2d95b
Victor Payno <vpayno@users.noreply.github.com>
2025-04-08 08:33:38 -0700

    nix(formatter): use gh:vpayno/nix-treefmt-conf

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 Nix flake with `devShells` for multiple programming languages and multiple
 versions.
+
+## linting & formatting
+
+Use `nix fmt` to lint and format the repo using
+`github:vpayno/nix-treefmt-conf`.
--- flake.nix
+++ flake.nix
@@ -1,15 +1,32 @@
+# flake.nix
 {
-  description = "A very basic flake";
+  description = "Flake with devshells";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    systems.url = "github:vpayno/nix-systems-default";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+
+    treefmt-conf = {
+      url = "github:vpayno/nix-treefmt-conf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs =
+    {
+      nixpkgs,
+      systems,
+      flake-utils,
+      treefmt-conf,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      formatter = treefmt-conf.formatter.${system};
+    });
 }
