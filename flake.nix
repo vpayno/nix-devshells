3d38d1770600fe5993521d06029c6210b184d1f1
Victor Payno <vpayno.oss@gmail.com>
2026-03-15 20:30:15 -0700

    nix(develop/rust): add 1.94.0, remove 1.87.0

--- flake.nix
+++ flake.nix
@@ -52,13 +52,13 @@
           # can't decide if I want all the versions from packages.x86_64-linux.rust_1_x or just a select few
           # just adding the last 5 versions for now
           rustVersions = [
-            "1.87.0"
             "1.88.0"
             "1.89.0"
             "1.90.0"
             "1.91.1"
             "1.92.0"
             "1.93.1"
+            "1.94.0"
           ];
 
           rustLabels = builtins.map getShellLabel context.rustVersions;
