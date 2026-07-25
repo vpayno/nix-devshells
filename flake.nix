9e487e8a62b5a91a8d709649ef8b96db4da9cf2f
Victor Payno <vpayno.oss@gmail.com>
2026-07-25 13:28:25 -0700

    nix: bump flake version to 20260725.0.0

--- flake.nix
+++ flake.nix
@@ -55,7 +55,7 @@
       system:
       let
         pname = "nix-devshells";
-        version = "20260723.0.0";
+        version = "20260725.0.0";
         name = "${pname}-${version}";
 
         flake_repo_url = "github:vpayno/nix-devshells";
